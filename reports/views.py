9de4e5cad896d7b783cd3ed121c6fddce939d3ed
evgeny.arhipov <evgeny.arhipov@simbirsoft.com>
2025-01-12 16:03:14 +0300

    добавил мин, макс, авг выручку

--- reports/views.py
+++ reports/views.py
@@ -9,6 +9,9 @@ from datetime import datetime
 @permission_required('reports')
 def generate_report(request):
     total_amount = 0
+    min_amount = None
+    max_amount = None
+    avg_amount = None
     start_date = None
     end_date = None
     records = None
@@ -23,7 +26,11 @@ def generate_report(request):
                 service_date__lte=end_date
             )
 
-            total_amount = sum(record.final_price for record in records)
+            if records:
+                total_amount = sum(record.final_price for record in records)
+                min_amount = min(record.final_price for record in records)
+                max_amount = max(record.final_price for record in records)
+                avg_amount = total_amount / len(records)
 
         except ValueError:
             return HttpResponse("Ошибка: Неверный формат даты!", status=400)
@@ -31,6 +38,9 @@ def generate_report(request):
     return render(request, 'reports/reports.html', {
         'records': records,
         'total_amount': total_amount,
+        'min_amount': min_amount,
+        'max_amount': max_amount,
+        'avg_amount': avg_amount,
         'start_date': start_date,
         'end_date': end_date
     })
