297a07fe07f911ac5de54a5b72665501870436ce
evgeny.arhipov <evgeny.arhipov@simbirsoft.com>
2025-01-16 22:48:51 +0300

    новая логика начисления зп

--- payroll/views.py
+++ payroll/views.py
@@ -29,12 +29,23 @@ def payroll_view(request):
         month_number = form.cleaned_data['month']
         selected_month = MONTH_TRANSLATIONS[calendar.month_name[int(month_number)]]
         employees = Employee.objects.all()
+
         for employee in employees:
-            employee.accrued = employee.salary  # Начислено
-            employee.deducted = round(employee.salary * 0.118, 2)  # Удержано (11.8%)
-            employee.to_pay = employee.accrued - employee.deducted  # К выдаче
-            employee.child_sum = employee.children * 1400  # Сумма на детей
-            employee.total_with_children = employee.to_pay + employee.child_sum  # Итоговая сумма
+            employee.accrued = employee.salary
+
+            if employee.children < 3:
+                employee.child_sum = employee.children * 1400
+            else:
+                employee.child_sum = 2800 + (employee.children - 2) * 3000
+
+            if employee.accrued - employee.child_sum < 280000:
+                employee.deducted = round((employee.accrued - employee.child_sum) * 0.13, 2)
+            else:
+                employee.deducted = round(employee.accrued * 0.13, 2)
+
+            employee.to_pay = employee.accrued - employee.deducted
+
+            employee.total_with_children = employee.to_pay + employee.child_sum
 
     return render(request, 'payroll/payroll.html', {
         'form': form,
